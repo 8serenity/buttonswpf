d3bd838f5ae84a024dc9d2c349c9cf1da057c8df
Niyaz <mukhamedya.n@gmail.com>
2018-01-28 22:08:51 +0600

    finish hw

---
+++ Buttons/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Создать приложение с кнопками трёх разных форм. Формы создавать посредством шаблонов.
+
+namespace Buttons
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Click2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 2 clicked");
+        }
+
+        private void Click1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 1 clicked");
+        }
+
+        private void Click3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button 3 clicked");
+        }
+    }
+}
